2d739859ccf908973ab6508806f39a4a7368dfeb
COR Students <corstudents@Matts-MBP-109.localdomain>
2018-11-16 12:08:36 -0500

    working api

--- scripts.js
+++ scripts.js
@@ -1,16 +1,33 @@
 
-$( document ).ready(function() { 
+const showManeuvers = (data) => {
+
+    const maneuvers = data.route.legs[0].maneuvers
+
+    maneuvers.forEach(function (entry) {
+        console.log(entry.narrative);
+        const move = `<li>${entry.narrative}</li>`
+        console.log(move)
+        $("#directions-list").append(move)
+    })
+
+}
+
+
+$(document).ready(function () {
+    // const apiKey = "AIzaSyCsmcUMF-5Tu24AMaOvHgymX6zEdlhZUsA"
+    const directionsurl = "http://open.mapquestapi.com/directions/v2/route?key=laOe6DWBIQGVlWUQSlAnz8o7uUilhary&from=Clarendon%20Blvd,Arlington,VA&to=2400+S+Glebe+Rd,+Arlington,+VA"
+
+
 
     $.ajax({
-        url:"http://open.mapquestapi.com/directions/v2/route?key=KEY&from=Clarendon Blvd,Arlington,VA&to=2400+S+Glebe+Rd,+Arlington,+VA",
+        url: directionsurl,
 
 
-success: function(data){
-    console.log(data)
-}
+        success: showManeuvers
     })
-// error:
-    // })
+
+    // error: funtion
+    //     })
 
 
 
